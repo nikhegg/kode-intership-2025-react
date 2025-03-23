96072e8f989e46ea294675d32af56d80daca5658
nikhegg <77997488+nikhegg@users.noreply.github.com>
2025-03-23 22:11:05 +0200

    Preps for Pages deploy

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createHashRouter } from "react-router-dom"
 import MainPage from "../components/MainPage"
 import UserDetails from "../components/details/UserDetails"
 import PageNotFound from "../components/404"
 import PageLayout from "../components/shared/PageLayout"
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         element: <PageLayout />,
         children: [
